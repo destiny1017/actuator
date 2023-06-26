e1f364f7d39b5ad72ade92ecd0bc79d020c11713
KDH_Notebook <destiny1017@nate.com>
2023-06-26 22:50:56 +0900

    모니터링 메트릭 활용

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,9 +1,19 @@
 package hello;
 
+import hello.order.v1.OrderConfigV1;
+import hello.order.v2.OrderConfigV2;
+import hello.order.v3.OrderConfigV3;
+import hello.order.v4.OrderConfigV4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@Import(OrderConfigV0.class)
+//@Import(OrderConfigV1.class)
+//@Import(OrderConfigV2.class)
+//@Import(OrderConfigV3.class)
+@Import(OrderConfigV4.class)
+@SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
     public static void main(String[] args) {
